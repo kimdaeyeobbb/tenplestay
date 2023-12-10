30d94582e4875b5eac1993c796acf6a611272c7b
kimdaeyeobbb <kimdyk2@hanmail.net>
2023-12-10 15:05:00 +0900

    fix: 색상 정보 수정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,34 +4,38 @@ module.exports = {
   theme: {
     colors: {
       grayscale: {
-        bg: '#01030C',
+        bg: '#101013',
         50: '#F8FAFC',
         400: '#94A3B8',
         700: '#334155',
         800: '#1E293B',
-        900: '#0F172A',
+        900: '#0A0A0D',
       },
       brand: {
-        200: '#BFDBFE',
-        primarylight: '#60A5FA',
-        primary: '#2672ED',
-        primarydard: '#1F43B8',
+        200: '#BFC4FE',
+        primarylight: '#7B85FA',
+        primary: '#4353FF',
+        primarydard: '#1F2CB8',
       },
       error: {
         error: '#E82F2F',
         dark: '#B9880C',
       },
       warning: {
-        primary: '#FCC70B',
-        primarybold: '#D97706',
+        primary: '#F2DF35',
+        primarybold: '#B9880C',
       },
       success: {
         success: '#0EB260',
         successdark: '#085E33',
       },
       point: {
-        500: '#CF158F',
-        700: '#8F1465',
+        pink500: '#CF158F',
+        pink700: '#8F1465',
+        purple600: '#6D3AED',
+        purple800: '#4B21B6',
+        orange600: '#FF6F31',
+        orange800: '#963C16',
       },
     },
     extend: {},
