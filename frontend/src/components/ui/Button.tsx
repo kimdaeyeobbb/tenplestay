df787509c476f5b41b535a7133374e1f093a1388
kimdaeyeobbb <kimdyk2@hanmail.net>
2023-12-12 08:54:17 +0900

    feature: 버튼 컴포넌트 구현

---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,49 @@
+interface ButtonProps {
+  borderRadius?: string;
+  borderWidth?: number;
+  borderColor?: string;
+  height: string;
+  width: string;
+  txtColor?: string;
+  bgColor?: string;
+  btnTxt: React.ReactNode;
+  type: 'submit' | 'reset' | 'button';
+  value?: string;
+  onClick: () => void;
+}
+
+const Button = ({
+  borderRadius,
+  borderWidth,
+  borderColor,
+  height,
+  width,
+  txtColor,
+  bgColor,
+  btnTxt,
+  type,
+  value,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <>
+      <button
+        type={type}
+        value={value}
+        onClick={onClick}
+        className={`
+        rounded-${borderRadius} 
+        border-${borderWidth} 
+        border-${borderColor} 
+        h-${height} 
+        w-${width} 
+        text-${txtColor} 
+        bg-${bgColor}`}
+      >
+        {btnTxt}
+      </button>
+    </>
+  );
+};
+
+export default Button;
