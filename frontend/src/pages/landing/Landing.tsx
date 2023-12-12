c3c7720327044770aedbd468911d86139fa17763
kimdaeyeobbb <kimdyk2@hanmail.net>
2023-12-12 17:06:11 +0900

    feat: 랜딩 페이지추가

---
+++ frontend/src/pages/landing/Landing.tsx
@@ -0,0 +1,13 @@
+import GoogleLoginButton from '../../components/ui/Button/GoogleLoginButton';
+
+const Landing = () => {
+  return (
+    <section>
+      <h1>랜딩페이지</h1>
+      <div>랜딩 페이지 내용 작성 </div>
+      <GoogleLoginButton />
+    </section>
+  );
+};
+
+export default Landing;
