a4f6348f6819c507e76728a13461e5531645ae6c
Nuung <qlgks1@naver.com>
2023-12-13 15:40:30 +0900

    modify: scraping testing 및 운영계 고도화 진행중 7차

--- backend/apps/scraping/admin.py
+++ backend/apps/scraping/admin.py
@@ -1,19 +1,41 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import ScrapingUrl, ScrapingLog, ScrapingGroup
 
 
 @admin.register(ScrapingUrl)
 class ScrapingUrlAdmin(admin.ModelAdmin):
-    list_display = ("website", "user", "scraping_group", "created_at", "updated_at")
+    list_display = (
+        "website",
+        "user",
+        "last_scraping_log_str",
+        "scraping_group",
+        "main_noti_platform",
+        "sub_noti_platform",
+        "created_at",
+        "updated_at",
+    )
     list_filter = ("user", "scraping_group", "is_static")
     search_fields = ("website", "keywords")
+
+    def last_scraping_log_str(self, obj):
+        return format_html(
+            "<div>{}</div>",
+            obj.last_scraping_log.__str__() if obj.last_scraping_log else "-",
+        )
 
 
 @admin.register(ScrapingLog)
 class ScrapingLogAdmin(admin.ModelAdmin):
-    list_display = ("result", "is_error", "created_at", "updated_at")
+    list_display = ("result_str", "is_error", "created_at", "updated_at")
     list_filter = ("is_error",)
     search_fields = ("result",)
+
+    def result_str(self, obj):
+        return format_html("<div>{}</div>", obj.__str__())
+
+    result_str.short_description = "Scraping Result"
 
 
 @admin.register(ScrapingGroup)
