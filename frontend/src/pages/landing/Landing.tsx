a2b4dab90065b7fdffa252c3eed6c69e79da019c
Nuung <qlgks1@naver.com>
2023-12-15 17:54:20 +0900

    modify: home routing fix v2

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -4,26 +4,6 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Landing = () => {
-  useEffect(() => {
-    const location = useLocation();
-    console.log('위치 확인: ', location);
-  }, []);
-  // useEffect(() => {
-  //   const pathname = new URLSearchParams(window.location.pathname);
-  //   console.log("hostname뒤의 '/' 문자 뒤의 경로: ", pathname);
-
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   console.log('urlparmas 확인: ', urlParams);
-
-  //   const code = urlParams.get('code');
-  //   const scope = urlParams.get('scope');
-  //   console.log('코드 확인: ', 'scope 확인: ', scope);
-
-  //   if (code && scope) {
-  //     const platform = scope.includes('google') ? 'google' : 'kakao';
-  //     oauthApicallByCode(code, platform);
-  //   }
-  // }, []);
 
   return (
     <section>
