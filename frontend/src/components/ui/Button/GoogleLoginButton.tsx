b74534b577a9375a3ab01a1455617fa64846e7d3
Nuung <qlgks1@naver.com>
2023-12-15 17:56:06 +0900

    modify: home routing fix v3

--- frontend/src/components/ui/Button/GoogleLoginButton.tsx
+++ frontend/src/components/ui/Button/GoogleLoginButton.tsx
@@ -1,18 +1,10 @@
-import { useEffect } from 'react';
 import { getGoogleAuthURL } from '../../../apis/googleLogin';
-import { useLocation } from 'react-router-dom';
 
 // 버튼 클릭시 Google OAuth 로그인 페이지로 리디렉션
 const GoogleLoginButton = () => {
   const handleGoogleLogin = () => {
-    useEffect(() => {
-      const location = useLocation();
-      console.log('위치 확인: ', location);
-    }, []);
-
     const authURL = getGoogleAuthURL();
-
-    window.location.href = authURL;
+    // window.location.href = authURL;
     console.log('URL 확인: ', authURL);
   };
 
