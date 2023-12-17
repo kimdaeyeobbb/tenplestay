2dbdc229e2b480806e2be69ebc03429b645678ee
Nuung <qlgks1@naver.com>
2023-12-17 15:35:31 +0900

    feature: messaging while true

--- backend/worker/messaging.py
+++ backend/worker/messaging.py
@@ -88,44 +88,52 @@ async def send_noti(message_moduel: MessagingModule, noti_with_scraping: dict):
 
 
 async def main():
-    # while True:
-    try:
-        mm = MessagingModule("third-party", settings)
-        rep = Repository(DB_URL)
-        await rep.initialize()
-        connection_successful = await rep.test_connection()
-        log.info(f"Connection Successful: {connection_successful}")
+    while True:
+        try:
+            mm = MessagingModule("third-party", settings)
+            rep = Repository(DB_URL)
+            await rep.initialize()
+            connection_successful = await rep.test_connection()
+            log.info(f"Connection Successful: {connection_successful}")
 
-        noti_with_scraping_list: list[dict] = await rep.get_all_noti_with_scarping_url()
-        tasks = [
-            asyncio.create_task(send_noti(mm, noti_with_scraping))
-            for noti_with_scraping in noti_with_scraping_list
-        ]
-        noti_results = await asyncio.gather(*tasks)
+            noti_with_scraping_list: list[
+                dict
+            ] = await rep.get_all_noti_with_scarping_url()
+            tasks = [
+                asyncio.create_task(send_noti(mm, noti_with_scraping))
+                for noti_with_scraping in noti_with_scraping_list
+            ]
+            if not tasks:
+                log.info("전송할 알람이 아무것도 없습니다.")
+                await rep.close()
+                continue
 
-        # 결과들을 모아서 bulk create (bulk insert into)
-        bulk_send_log_data = list()
-        for noti_with_scraping, noti_result in zip(
-            noti_with_scraping_list, noti_results
-        ):
-            noti_with_scraping: dict
-            noti_result: dict
-            bulk_send_log_data.append(
-                (
-                    noti_with_scraping["id"],
-                    json.dumps(noti_result),
+            # 노티 발송!
+            noti_results = await asyncio.gather(*tasks)
+
+            # 결과들을 모아서 bulk create (bulk insert into)
+            bulk_send_log_data = list()
+            for noti_with_scraping, noti_result in zip(
+                noti_with_scraping_list, noti_results
+            ):
+                noti_with_scraping: dict
+                noti_result: dict
+                bulk_send_log_data.append(
+                    (
+                        noti_with_scraping["id"],
+                        json.dumps(noti_result),
+                    )
                 )
-            )
-            await rep.update_noti_clear(noti_with_scraping["id"])
+                await rep.update_noti_clear(noti_with_scraping["id"])
 
-        # bulk insert query 본체
-        await rep.bulk_create_noti_send_log(bulk_send_log_data)
-        log.info(f"{len(noti_results)} 개 noti clear")
-        await rep.close()
-    except Exception as e:
-        log.error(f"main error >> {e}, {e.__class__}")
-    finally:
-        await sleep(600)  # Sleep for 600 seconds (10 minutes)
+            # bulk insert query 본체
+            await rep.bulk_create_noti_send_log(bulk_send_log_data)
+            log.info(f"{len(noti_results)} 개 noti clear")
+            await rep.close()
+        except Exception as e:
+            log.error(f"main error >> {e}, {e.__class__}")
+        finally:
+            await sleep(60 * 1)  # Sleep for 60 seconds (1 minutes)
 
 
 if __name__ == "__main__":
